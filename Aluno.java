7d8905480989c157c7396b5805731681042fbb73
Caio <caiobueno.cb@gmail.com>
2020-03-14 12:13:31 -0300

    Update Aluno.java
    Inserção de GET

--- Aluno.java
+++ Aluno.java
@@ -2,5 +2,15 @@ public class Aluno{
    private String nome;
    private int id;
    
+   
+   public String getNome() {
+	   return nome;
+   }
+   
+   public int getId() {
+	   return id;
+	   
+   }
+   
 }
    
