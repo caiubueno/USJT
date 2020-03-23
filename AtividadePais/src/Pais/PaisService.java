42a215f19dad482f562cde6a501f1f5925dcfaac
caiubueno <54920760+caiubueno@users.noreply.github.com>
2020-03-22 23:41:01 -0300

    Correção atividade

--- AtividadePais/src/Pais/PaisService.java
+++ AtividadePais/src/Pais/PaisService.java
@@ -1,19 +1,21 @@
-package atividadePais;
-
-import java.util.ArrayList;
+package pais;
 
 public class PaisService {
 	PaisDAO dao = new PaisDAO();
-	
-	@SuppressWarnings({ "rawtypes" })
-	public ArrayList getPaisMaisHab() {
-		return PaisDAO.buscaPaisMaisHab();	
-	}
-	
-	@SuppressWarnings("rawtypes")
-	public ArrayList getPaisMenor() {
-		return PaisDAO.buscaPaisMenor();	
-	}
-	
 
+	public int criar(Pais pais) {
+		return dao.criar(pais);
+	}
+
+	public void atualizar(Pais pais){
+		dao.atualizar(pais);
+	}
+
+	public void excluir(int id){
+		dao.excluir(id);
+	}
+
+	public Pais carregar(int id){
+		return dao.carregar(id);
+	}
 }
