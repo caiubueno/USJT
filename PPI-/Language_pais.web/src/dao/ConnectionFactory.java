965afdbc1da49bef0b673c9cfa9ed7937c8e733b
caiubueno <caiobueno.cb@gmail.com>
2020-05-12 21:57:46 -0300

    correção do BD

--- PPI-/Language_pais.web/src/dao/ConnectionFactory.java
+++ PPI-/Language_pais.web/src/dao/ConnectionFactory.java
@@ -16,7 +16,7 @@ public class ConnectionFactory {
 	// Obtem conexção com o banco de dados
 	public static Connection obtemConexao() throws SQLException {
 		return DriverManager
-				.getConnection("jdbc:mysql://localhost:3306/mundo?useTimezone=true&serverTimezone=America/Sao_Paulo&user=root&password=7849516230Ee");
+				.getConnection("jdbc:mysql://localhost:3306/mundo?useTimezone=true&serverTimezone=America/Sao_Paulo&user=root&password=819229668");
 	}
 
 }
